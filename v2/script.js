e224540b0946c9d1aa49c2849428eb441634f0a2
Diamons-Gold <hrombat@gmail.com>
2021-06-06 09:24:23 +0200

    update

--- v2/script.js
+++ v2/script.js
@@ -1,16 +1,26 @@
-var root = document.querySelector(':root')
-var rootStyle = getComputedStyle(root);
+var older;
+var set;
 
 function select(id) {
     var element = document.getElementById(id)
+    var older = document.getElementById(window.older)
+
     if (element.classList.contains('select') == 1) {
         element.classList.remove('select');
+        element.style.zIndex = '10';
+        window.older = id
+        window.set = 'false'
     } else {
-        element.classList.add('select')
-
-        var x = element.getBoundingClientRect().left;
-        root.style.setProperty('--x', x + 'px');
-        var y = element.getBoundingClientRect().top;
-        root.style.setProperty('--y', y + 'px');
+        if (!set) {
+            element.classList.add('select')
+            element.style.zIndex= '10';
+        } else if (window.set == 'false') {
+            element.classList.add('select')
+            element.style.zIndex= '10';
+        }
+        if (older && window.older != id) {
+            older.style.zIndex= '1';
+        }
+        window.set = 'true'
     }
 }
